ec73e76f0256fe54d90bf90619ae6f4b3b44a486
nf1213 <nicole@oviahealth.com>
2018-12-12 09:28:19 -0500

    iOS project

---
+++ native/KotlinNativeIOS/KotlinNativeIOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KotlinNativeIOS
+//
+//  Created by Nicole Felch on 11/14/18.
+//  Copyright © 2018 Nicole Felch. All rights reserved.
+//
+
+import UIKit
+import SharedCode
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createApplicationScreenMessage()
+        view.addSubview(label)
+    }
+}
+
